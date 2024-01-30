933c63304a8ca35af6d25313895de47e1d770349
mas0110 <masoudmohammed13@gmail.com>
2024-01-30 14:52:53 +0100

    lista contatti dinamica

--- main.js
+++ main.js
@@ -5,6 +5,40 @@ const {
 createApp({
     data() {
         return {
+            amici:[
+                {
+                    name:'Michele',
+                    img:'./img/avatar_1.jpg'
+                },
+                {
+                    name:'fabio',
+                    img:'./img/avatar_3.jpg'
+                },
+                {
+                    name:'claudia',
+                    img:'./img/avatar_4.jpg'
+                },
+                {
+                    name:'Samuele',
+                    img:'./img/avatar_5.jpg'
+                },
+                {
+                    name:'Alessandro B.',
+                    img:'./img/avatar_6.jpg'
+                },
+                {
+                    name:'Alessandro L.',
+                    img:'./img/avatar_7.jpg'
+                },
+                {
+                    name:'Federica',
+                    img:'./img/avatar_io.jpg'
+                },
+                {
+                    name:'Davide',
+                    img:'./img/avatar_8.jpg'
+                },
+            ]
         }
     },
     created(){
